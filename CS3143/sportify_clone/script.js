3948e8e7e02f0f6ef594b834208b228b2dbc81d9
Aaryan Chipkar <97173368+Aaryan170202@users.noreply.github.com>
2023-06-27 22:09:35 +0530

    Add files via upload

---
+++ CS3143/sportify_clone/script.js
@@ -0,0 +1,15 @@
+console.log("Welcome to spotify")
+let songs = [
+    {songName: "1", filePath: "c_w_h/songs/1.mp3", cover: "c_w_h/covers/1.mp3"},
+    {songName: "2", filePath: "c_w_h/songs/2.mp3", cover: "c_w_h/covers/2.mp3"},
+    {songName: "3", filePath: "c_w_h/songs/3.mp3", cover: "c_w_h/covers/3.mp3"},
+    {songName: "4", filePath: "c_w_h/songs/4.mp3", cover: "c_w_h/covers/4.mp3"},
+    {songName: "5", filePath: "c_w_h/songs/5.mp3", cover: "c_w_h/covers/5.mp3"},
+    {songName: "6", filePath: "c_w_h/songs/6.mp3", cover: "c_w_h/covers/6.mp3"},
+    {songName: "7", filePath: "c_w_h/songs/7.mp3", cover: "c_w_h/covers/7.mp3"},
+    {songName: "8", filePath: "c_w_h/songs/8.mp3", cover: "c_w_h/covers/8.mp3"},
+    {songName: "9", filePath: "c_w_h/songs/9.mp3", cover: "c_w_h/covers/9.mp3"},
+    {songName: "10", filePath: "c_w_h/songs/10.mp3", cover: "c_w_h/covers/10.mp3"}
+]
+let audioElement = new Audio('c_w_h/songs/1.mp3');
+// audioElement.play();
